f07273c20c2f5a5113ddfec6fe5da677697470f2
Sam Blowes <samuelblowes@outlook.com>
2023-07-31 09:32:57 -0500

    Update rules.mk
    Reverting to what worked

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -7,12 +7,11 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = no
 RGB_MATRIX_DRIVER = WS2812
-VIA_ENABLE = yes
 LTO_ENABLE = yes
 
 # if firmware size over limit, try this option
