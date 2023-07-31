8d5ae95b5739898af49216f29df7431850580353
Sam Blowes <samuelblowes@outlook.com>
2023-07-31 08:30:28 -0500

    Update rules.mk

--- keyboards/crkbd/rules.mk
+++ keyboards/crkbd/rules.mk
@@ -12,6 +12,7 @@ RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 RGB_MATRIX_ENABLE = yes
 RGB_MATRIX_DRIVER = WS2812
+VIA_ENABLE = yes
 LTO_ENABLE = yes
 
 # if firmware size over limit, try this option
