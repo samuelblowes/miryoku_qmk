cfda06b59407a9cd66341f18a6bfd6a55da18dca
Sam Blowes <samuelblowes@outlook.com>
2023-07-31 21:10:09 -0500

    Update rules.mk

--- keyboards/crkbd/keymaps/default/rules.mk
+++ keyboards/crkbd/keymaps/default/rules.mk
@@ -2,4 +2,5 @@ MOUSEKEY_ENABLE = yes    # Mouse keys
 RGBLIGHT_ENABLE = yes    # Enable WS2812 RGB underlight.
 OLED_ENABLE     = yes
 OLED_DRIVER     = SSD1306
+VIA_ENABLE		= yes
 LTO_ENABLE      = yes
